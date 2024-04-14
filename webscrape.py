3b78640f88144e50b118d8b4802ec935501b89a4
“Malgorzata <malgorzata.k.kuchna@gmail.com>
2024-04-14 12:37:03 +0200

    Add data structure to store job information

---
+++ webscrape.py
@@ -0,0 +1,11 @@
+
+jobs = {
+    'company': 'Company Name',
+    'title': 'Job Title',
+    'department': 'Department Name',
+    'location': 'Location',
+    'experience': 'Experience Level',
+    'responsibilities': 'Responsibilities',
+    'basic_requirements': 'Basic Qualifications and Experience',
+    'preferred_requirements': 'Preferred Qualifications and Experience'
+}
