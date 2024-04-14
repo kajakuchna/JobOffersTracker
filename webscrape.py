10921f9bdaa02e0e2151e38922a9559af4c64862
“Malgorzata <malgorzata.k.kuchna@gmail.com>
2024-04-14 14:00:55 +0200

    Add function for saving jobs to CSV file

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,6 @@
+import csv
 
-jobs = {
+job_offer = {
     'company': 'Company Name',
     'title': 'Job Title',
     'department': 'Department Name',
@@ -9,3 +10,14 @@ jobs = {
     'basic_requirements': 'Basic Qualifications and Experience',
     'preferred_requirements': 'Preferred Qualifications and Experience'
 }
+
+
+def save_jobs_to_csv(jobs, filename):
+    fieldnames = ['company', 'title', 'department', 'location', 'experience', 'responsibilities', 'basic_requirements',
+                  'preferred_requirements']
+
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for job in jobs:
+            writer.writerow(job)
